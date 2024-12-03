687b51f200f9755261c5c32e0d01be4cc8c287b7
yowka <stasskirnevskiy@gmail.com>
2024-12-03 18:14:05 +0700

    Маршрутизация

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/test', function () {
+    return "Welcome";
 });
+Route::get('/dir/test', function () {
+    return "Welcome to my group";
+});
+Route::get('/user/{id}', function ($id) {
+    return 'Пользователь ' . $id;
+});
+Route::get('/post/{id}', function ($id, $surname = "Скирневский", $name = "Стас") {
+    return $surname . ' ' . $name;
+});
+Route::get('/posts/{id}', function ($id) {
+    return 'пост ' . $id;
+})->where('id', '[0-9]+');
